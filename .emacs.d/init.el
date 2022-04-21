60ea7eadefbdbc8aba1560ba592b34abd1b49e99
Taylor Murias <tmurias@sierrawireless.com>
2022-04-21 11:19:57 -0700

    Emacs config - removed custom section from common config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -126,20 +126,3 @@
   :ensure t
   :config
   (editorconfig-mode 1))
-
-
-;; ------------------------- DON'T TOUCH ------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(treemacs-projectile projectile projectile-mode lua-mode treemacs-evil treemacs lsp-mode evil-collection evil general which-key firecode-theme gruvbox-theme use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-;; --------------------------------------------------------------------
