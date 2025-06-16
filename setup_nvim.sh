65ece6baeb10bd11db25c1ca5cb93c834285360a
Taylor Murias <taylor@murias.ca>
2025-06-15 17:17:38 -0700

    Neovim setup script - replace echo with printf
    Using echo with the -e flag to handle escape characters is not
    compatible with dash (default shell on Ubuntu 22.04). Replace echo with
    printf so it works the same with all shells.

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -5,17 +5,17 @@ mkdir -p ~/.config/nvim
 touch ~/.config/nvim/init.vim
 if grep -Fq "source ~/dotfiles/.config/nvim/init.vim" ~/.config/nvim/init.vim
 then
-	echo '~/.config/nvim/init.vim already links to managed config'
+	printf '~/.config/nvim/init.vim already links to managed config\n'
 else
-	echo 'Linking ~/.config/nvim/init.vim to managed nvim config'
-	echo -e "\nsource ~/dotfiles/.config/nvim/init.vim\n" >> ~/.config/nvim/init.vim
+	printf 'Linking ~/.config/nvim/init.vim to managed nvim config\n'
+	printf "\nsource ~/dotfiles/.config/nvim/init.vim\n" >> ~/.config/nvim/init.vim
 fi
 
 # Install plugins
 python3 ./nvim_plugin_install_update.py
 
 # Copy over lua files
-echo 'Added lua config to ~/.config/nvim/lua/taylor'
+printf 'Added lua config to ~/.config/nvim/lua/taylor\n'
 mkdir -p ~/.config/nvim/lua/taylor
 cp .config/nvim/lua/taylor/lspconfig.lua ~/.config/nvim/lua/taylor/lspconfig.lua
 cp .config/nvim/lua/taylor/treesitterconfig.lua ~/.config/nvim/lua/taylor/treesitterconfig.lua
