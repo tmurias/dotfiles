5f0764d7446eb3d380b7242656bcb12aea339bda
Taylor Murias <taylor@murias.ca>
2026-03-19 23:18:10 -0700

    setup_nvim.sh improvements
    - dotfiles dir path is detected instead of hardcoded "~/dotfiles"
    - lua files are symlinked instead of copied

--- setup_nvim.sh
+++ setup_nvim.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Link config
 mkdir -p ~/.config/nvim
 touch ~/.config/nvim/init.vim
-if grep -Fq "source ~/dotfiles/.config/nvim/init.vim" ~/.config/nvim/init.vim
+if grep -Fq "source $SCRIPT_DIR/.config/nvim/init.vim" ~/.config/nvim/init.vim
 then
 	printf '~/.config/nvim/init.vim already links to managed config\n'
 else
 	printf 'Linking ~/.config/nvim/init.vim to managed nvim config\n'
-	printf "\nsource ~/dotfiles/.config/nvim/init.vim\n" >> ~/.config/nvim/init.vim
+	printf "\nsource $SCRIPT_DIR/.config/nvim/init.vim\n" >> ~/.config/nvim/init.vim
 fi
 
 # Install plugins
 python3 ./nvim_plugin_install_update.py
 
-# Copy over lua files
-printf 'Added lua config to ~/.config/nvim/lua/taylor\n'
+# Link lua files
 mkdir -p ~/.config/nvim/lua/taylor
-cp .config/nvim/lua/taylor/lspconfig.lua ~/.config/nvim/lua/taylor/lspconfig.lua
-cp .config/nvim/lua/taylor/treesitterconfig.lua ~/.config/nvim/lua/taylor/treesitterconfig.lua
+ln -sf "$SCRIPT_DIR/.config/nvim/lua/taylor/lspconfig.lua" \
+	~/.config/nvim/lua/taylor/lspconfig.lua
+ln -sf "$SCRIPT_DIR/.config/nvim/lua/taylor/treesitterconfig.lua" \
+	~/.config/nvim/lua/taylor/treesitterconfig.lua
+printf 'Added lua config to ~/.config/nvim/lua/taylor\n'
